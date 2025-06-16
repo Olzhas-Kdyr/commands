3b338d4e38e437928e5b8046a933b58dff923f4e
olzhas0986 <138654744+olzhas0986dev@users.noreply.github.com>
2025-06-16 08:32:58 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,27 +1,48 @@
 rm -rf .repo/local_manifests; \
 
+# Repo Init
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 
+# repo sync
 /opt/crave/resync.sh; \
 
+# rm -rf old repos
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
+# Device Trees
 git clone https://gitlab.com/rik-x777/device_xiaomi_munch device/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
+
+# Vendor Trees
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
+
+# Munch Firmware
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+
+# Kernel Tree
 git clone https://github.com/olzhas0986dev/kernel_xiaomi_sm8250.git -b bpf kernel/xiaomi/sm8250; \
+
+# KernelSU-Next
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
+
+# Hardware Xiaomi
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi hardware/xiaomi; \
+
+# DeviceSettings
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+
+# ViPEE4AndroidFX
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+
+# Miui Camera
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 rm -rf vendor/lineage-priv/keys && rm -rf vendor/infinity-priv/keys; \
 
+# Build
 . build/envsetup.sh; \
 
 lunch lineage_munch-bp2a-user && m evolution; \
