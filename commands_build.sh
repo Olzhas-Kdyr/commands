fd303c6ce5057ceb16aaf1fbbe32663676b4d01d
olzhas0986 <138654744+olzhas0986dev@users.noreply.github.com>
2025-06-18 12:24:07 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -23,29 +23,24 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 # Device Trees
 git clone https://github.com/olzhas0986dev/android_device_xiaomi_munch.git device/xiaomi/munch; \
-#git clone  device/xiaomi/sm8250-common; \
 
 # Vendor Trees
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-#git clone  vendor/xiaomi/sm8250-common; \
 
 # Munch Firmware
-#git clone https://github.com/olzhas0986dev/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
+git clone https://github.com/olzhas0986dev/vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
 
 # Kernel Tree
 git clone https://github.com/olzhas0986dev/kernel_xiaomi_sm8250.git kernel/xiaomi/munch; \
 
-# KernelSU-Next
-#cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
-
-# Hardware
+# Hardware Xiaomi
 git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 
 # DeviceSettings
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
 # ViPER4AndroidFX
-#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+git clone https://github.com/olzhas0986dev/ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
 
 # MIUI Camera
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
@@ -54,4 +49,3 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 . build/envsetup.sh; \
 breakfast munch; \
 m superior
-#lunch lineage_munch-bp2a-user && m evolution
