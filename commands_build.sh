4d4678f5e8249c72b4cf3383d62eb953090165d8
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-06-25 16:22:08 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,7 +1,9 @@
 # rm -rf local manifests
+
 rm -rf .repo/local_manifests; \
 
 # Repo Init
+
 #repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
 #repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs; \
 #repo init -u https://github.com/PixelOS-AOSP/manifest.git -b sixteen --git-lfs; \
@@ -9,9 +11,11 @@ rm -rf .repo/local_manifests; \
 repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs; \
 
 # repo sync
+
 /opt/crave/resync.sh; \
 
 # rm -rf old repos
+
 rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
 rm -rf device/xiaomi/sm8250-common; \
@@ -25,30 +29,40 @@ rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 
 # Device Trees
+
 git clone https://github.com/Olzhas-Kdyr/los_device_xiaomi_munch device/xiaomi/munch; \
 git clone https://github.com/Olzhas-Kdyr/los_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common; \
 
 # Vendor Trees
+
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 
 # Kernel Tree
+
 git clone https://github.com/Olzhas-Kdyr/xiaomi_kernel_sm8250.git -b bpf kernel/xiaomi/sm8250; \
 
 # KernelSU-Next
+
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
 # Hardware
+
 git clone https://github.com/Olzhas-Kdyr/los_android_hardware_xiaomi.git hardware/xiaomi; \
 
 # DeviceSettings
+
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
 # ViPER4AndroidFX
+
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
 # MIUI Camera
+
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+
+# YAAP stuffs
 
 rm -rf device/qcom/common; \
 git clone https://github.com/Olzhas-Kdyr/device_qcom_common.git -b fifteen device/qcom/common; \
@@ -56,5 +70,6 @@ rm -rf vendor/yaap; \
 git clone https://github.com/Olzhas-Kdyr/vendor_yaap.git -b fifteen vendor/yaap; \
 
 # Build 
+
 . build/envsetup.sh; \
-lunch yaap_munch-user && m yaap
+lunch yaap_munch-userdebug && m yaap
