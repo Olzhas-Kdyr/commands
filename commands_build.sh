04139d42262aadeaec9129f04e6c51f014225b85
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-04 12:32:15 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,5 +1,5 @@
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Black-Iron-Project/manifest -b v15_QPR2 --git-lfs; \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 rm -rf out/target/product/munch; \
@@ -11,7 +11,7 @@ rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b munch-ksu-susfs kernel/xiaomi/munch; \
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xiaomi; \
@@ -19,9 +19,9 @@ git clone https://github.com/munch-devs/android_hardware_dolby.git hardware/dolb
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 . build/envsetup.sh; \
-blkilunch munch user && blki b; \
+lunch infinity_munch-user && mka bacon; \
 rm -rf out/target/product/vanilla out/target/product/gapps out/target/product/full_gapps; \
 cd out/target/product && mv munch vanilla && cd ../../..; \
-cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv gapps.txt lineage_munch.mk && cd ../../..; \
+cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv gapps.txt infinity_munch.mk && cd ../../..; \
 . build/envsetup.sh; \
-blkilunch munch user && blki b
+lunch infinity_munch-user && mka bacon
