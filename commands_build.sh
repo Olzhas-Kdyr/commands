d57565ebb0500438641206e4251b5a8745062057
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-05 00:42:56 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,7 +1,11 @@
 rm -rf .repo/local_manifests; \
+
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
+
 rm -rf prebuilts/clang/host/linux-x86; \
+
 /opt/crave/resync.sh; \
+
 rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
 rm -rf kernel/xiaomi/munch; \
@@ -11,17 +15,22 @@ rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
+
 git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b munch-ksu-susfs kernel/xiaomi/munch; \
+git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b munch-ksu kernel/xiaomi/munch; \
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xiaomi; \
 git clone https://github.com/munch-devs/android_hardware_dolby.git hardware/dolby; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://github.com/Olzhas-Kdyr/ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
+
 . build/envsetup.sh; \
 lunch infinity_munch-user && mka bacon; \
+
 rm -rf out/target/product/vanilla out/target/product/gapps out/target/product/full_gapps; \
 cd out/target/product && mv munch vanilla && cd ../../..; \
 cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv gapps.txt infinity_munch.mk && cd ../../..; \
+
 . build/envsetup.sh; \
 lunch infinity_munch-user && mka bacon
