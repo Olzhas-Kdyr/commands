c328658f6e3b29ce7b84fffb3e25a918b9f9e6c8
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-07 09:18:50 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,38 +1,39 @@
-#rm -rf .repo/local_manifests; \
+rm -rf .repo/local_manifests; \
 
 #repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 #repo init -u https://github.com/PixelOS-AOSP/manifest.git -b sixteen --git-lfs; \
+repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs
 
-#rm -rf prebuilts/clang/host/linux-x86; \
+rm -rf prebuilts/clang/host/linux-x86; \
 
-#/opt/crave/resync.sh; \
+/opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch; \
-#rm -rf device/xiaomi/munch; \
-#rm -rf kernel/xiaomi/munch; \
-#rm -rf vendor/xiaomi/munch; \
-#rm -rf vendor/xiaomi/munch-firmware; \
-#rm -rf hardware/xiaomi; \
-#rm -rf hardware/dolby; \
-#rm -rf vendor/xiaomi/miuicamera; \
-#rm -rf packages/resources/devicesettings; \
-#rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf device/xiaomi/munch; \
+rm -rf kernel/xiaomi/munch; \
+rm -rf vendor/xiaomi/munch; \
+rm -rf vendor/xiaomi/munch-firmware; \
+rm -rf hardware/xiaomi; \
+rm -rf hardware/dolby; \
+rm -rf vendor/xiaomi/miuicamera; \
+rm -rf packages/resources/devicesettings; \
+rm -rf packages/apps/ViPER4AndroidFX; \
 
-#git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b aosp device/xiaomi/munch; \
-#git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git -b 16 vendor/xiaomi/munch; \
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
-#git clone https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250.git kernel/xiaomi/munch; \
-#git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xiaomi; \
-#git clone https://github.com/munch-devs/android_hardware_dolby.git hardware/dolby; \
-#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
-#git clone https://github.com/Olzhas-Kdyr/ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b pixelage device/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git -b 16 vendor/xiaomi/munch; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware; \
+git clone https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250.git kernel/xiaomi/munch; \
+git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://github.com/munch-devs/android_hardware_dolby.git hardware/dolby; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://github.com/Olzhas-Kdyr/ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
 
-rm -rf packages/apps/ColumbusService; \
-git clone https://github.com/Olzhas-Kdyr/packages_apps_ColumbusService.git -b sixteen packages/apps/ColumbusService; \
+#rm -rf packages/apps/ColumbusService; \
+#git clone https://github.com/Olzhas-Kdyr/packages_apps_ColumbusService.git -b sixteen packages/apps/ColumbusService; \
 
 . build/envsetup.sh; \
-lunch aosp_munch-bp2a-user && mka bacon; \
+lunch pixelage_munch-bp2a-user && mka bacon; \
 
 #rm -rf out/target/product/vanilla out/target/product/gapps out/target/product/full_gapps; \
 #cd out/target/product && mv munch vanilla && cd ../../..; \
