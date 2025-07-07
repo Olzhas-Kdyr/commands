29d51613bd0d33d4bafc017d970c0f887a782b58
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-07 14:48:09 +0500

    Update and rename commands_build.sh to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 rm -rf .repo/local_manifests; \
 
 #repo init -u https://github.com/Black-Iron-Project/manifest -b v15_QPR2 --git-lfs; \
-repo init -u https://git.libremobileos.com/LMODroid/manifest.git -b fifteen --git-lfs; \
+#repo init -u https://git.libremobileos.com/LMODroid/manifest.git -b fifteen --git-lfs; \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 
 rm -rf prebuilts/clang/host/linux-x86; \
 
@@ -17,16 +18,16 @@ rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b munch-ksu kernel/xiaomi/munch; \
+git clone https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250 kernel/xiaomi/munch; \
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xiaomi; \
 git clone https://github.com/munch-devs/android_hardware_dolby.git hardware/dolby; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 . build/envsetup.sh; \
-lunch lineage_munch-bp1a-user && mka bacon; \
+lunch infinity_munch-user && mka bacon; \
 
 #rm -rf out/target/product/vanilla out/target/product/gapps out/target/product/full_gapps; \
 #cd out/target/product && mv munch vanilla && cd ../../..; \
