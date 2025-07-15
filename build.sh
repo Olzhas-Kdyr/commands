0163961c1035f63f0e6ff2fd3c0d36f8f823d57d
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-16 01:15:39 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Remove local_manifests
 
-#rm -rf .repo/local_manifests; \
+rm -rf .repo/local_manifests; \
 
 # repo init
 
@@ -9,18 +9,18 @@
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs; \
 #repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 #repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs; \
-#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
 
 # Fix clang error
-#rm -rf prebuilts/clang/host/linux-x86; \
+rm -rf prebuilts/clang/host/linux-x86; \
 
 # repo sync script
-#/opt/crave/resync.sh; \
+/opt/crave/resync.sh; \
 
 # Remove old device specific repos
 rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
-#rm -rf kernel/xiaomi/munch; \
+rm -rf kernel/xiaomi/munch; \
 rm -rf vendor/xiaomi/munch; \
 rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf hardware/xiaomi; \
@@ -42,7 +42,7 @@ git clone https://github.com/Olzhas-Kdyr/android_vendor_xiaomi_munch -b 16 vendo
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
 # Kernel Tree (N0Kernel)
-#git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b 16-ksu kernel/xiaomi/munch; \
+git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b 16-ksu kernel/xiaomi/munch; \
 
 # Hardware Xiaomi
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git -b 16 hardware/xiaomi; \
@@ -60,8 +60,8 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 git clone https://github.com/cyberknight777/android_packages_apps_FastCharge packages/apps/FastCharge; \
 
 # Build
-rm -rf frameworks/base; \
-git clone https://github.com/Olzhas-Kdyr/frameworks_base.git -b 16 frameworks/base; \
+#rm -rf frameworks/base; \
+#git clone https://github.com/Olzhas-Kdyr/frameworks_base.git -b 16 frameworks/base; \
 
 . build/envsetup.sh; \
 lunch infinity_munch-user && m bacon
