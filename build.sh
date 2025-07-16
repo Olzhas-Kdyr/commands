928b3505e1ef8f0b4234dfa07fb1acb37cf2289c
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-16 12:32:46 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Remove local_manifests
 
-rm -rf .repo/local_manifests; \
+#rm -rf .repo/local_manifests; \
 
 # repo init
 
@@ -9,23 +9,23 @@ rm -rf .repo/local_manifests; \
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs; \
 #repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 #repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs; \
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
+#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
 
 # Fix clang error
-rm -rf prebuilts/clang/host/linux-x86; \
+#rm -rf prebuilts/clang/host/linux-x86; \
 
 # repo sync script
-/opt/crave/resync.sh; \
+#/opt/crave/resync.sh; \
 
 # Remove old device specific repos
 rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
-rm -rf kernel/xiaomi/munch; \
-rm -rf vendor/xiaomi/munch; \
+#rm -rf kernel/xiaomi/munch; \
+#rm -rf vendor/xiaomi/munch; \
 rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf hardware/xiaomi; \
 rm -rf hardware/dolby; \
-rm -rf vendor/xiaomi/miuicamera; \
+#rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 rm -rf packages/apps/FastCharge; \
@@ -36,13 +36,13 @@ rm -rf packages/apps/FastCharge; \
 git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 
 # Vendor Tree
-git clone https://github.com/Olzhas-Kdyr/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
+#git clone https://github.com/Olzhas-Kdyr/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
 # Munch FW
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
 # Kernel Tree (N0Kernel)
-git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b 16-ksu kernel/xiaomi/munch; \
+#git clone https://github.com/munch-devs/kernel_xiaomi_munch.git -b 16-ksu kernel/xiaomi/munch; \
 
 # Hardware Xiaomi
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git -b 16 hardware/xiaomi; \
@@ -54,7 +54,7 @@ git clone https://github.com/Olzhas-Kdyr/android_hardware_dolby hardware/dolby; 
 git clone https://github.com/Olzhas-Kdyr/android_packages_resources_devicesettings.git packages/resources/devicesettings; \
 
 # MIUI Camera
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 # FastCharge
 git clone https://github.com/cyberknight777/android_packages_apps_FastCharge packages/apps/FastCharge; \
