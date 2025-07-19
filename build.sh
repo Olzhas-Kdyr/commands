12ae9b81845639139d56a9aa6231ee315b9a0b06
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-19 23:46:30 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,6 +56,9 @@ git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packa
 # MIUI Camera
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
+# ViPER4AndroidFX
+https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+
 # Build
 #rm -rf frameworks/base; \
 #git clone https://github.com/Olzhas-Kdyr/fb -b 16 frameworks/base; \
