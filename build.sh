6fb0c83191427e86ee61022a63186af06aa03f1f
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-22 09:56:32 +0500

    Luna

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Remove local_manifests
 
-#rm -rf .repo/local_manifests; \
+rm -rf .repo/local_manifests; \
 
 # repo init
 
@@ -10,13 +10,13 @@
 #repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 #repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs; \
 #repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
-#repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs; \
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs; \
 
 # Fix clang error
-#rm -rf prebuilts/clang/host/linux-x86; \
+rm -rf prebuilts/clang/host/linux-x86; \
 
 # repo sync script
-#/opt/crave/resync.sh; \
+/opt/crave/resync.sh; \
 
 # Remove old device specific repos
 rm -rf out/target/product/munch; \
@@ -33,7 +33,7 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 # Clone new device specific repos
 
 # Deivce Tree
-git clone https://github.com/PocoF4Trees/device_xiaomi_munch.git -b aosp device/xiaomi/munch; \
+git clone https://github.com/PocoF4Trees/device_xiaomi_munch.git -b 16 device/xiaomi/munch; \
 
 # Vendor Tree
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
@@ -65,8 +65,8 @@ git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/
 #rm -rf packages/modules/Connectivity; \
 #git clone https://github.com/Olzhas-Kdyr/packages_modules_Connectivity.git -b 16 packages/modules/Connectivity; \
 
-rm -rf vendor/qcom/opensource/vibrator; \
-git clone https://github.com/Olzhas-Kdyr/vendor_qcom_opensource_vibrator.git -b lineage-23.0 vendor/qcom/opensource/vibrator; \
+#rm -rf vendor/qcom/opensource/vibrator; \
+#git clone https://github.com/Olzhas-Kdyr/vendor_qcom_opensource_vibrator.git -b lineage-23.0 vendor/qcom/opensource/vibrator; \
 
 . build/envsetup.sh; \
-lunch aosp_munch-bp2a-user && mka bacon
+lunch lineage_munch-bp2a-user && m lunaris
