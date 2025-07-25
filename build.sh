6a86ca08db0fbb46d362d74a0a8963e698089fb7
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-07-25 14:31:33 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Remove local_manifests
 
-#rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests
 
 # repo init
 
@@ -8,66 +8,62 @@
 #repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
 #repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-#repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
+repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
 #repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 #repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
 # Fix clang error
-#rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf bionic
 
 # repo sync script
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 
 # Remove old device specific repos
-#mf=(
-#device/xiaomi
-#kernel/xiaomi
-#vendor/xiaomi
-#hardware/xiaomi
-#hardware/dolby
-#packages/resources/devicesettings
-#packages/apps/ViPER4AndroidFX
-#)
+mf=(
+device/xiaomi
+kernel/xiaomi
+vendor/xiaomi
+hardware/xiaomi
+hardware/dolby
+packages/resources/devicesettings
+packages/apps/ViPER4AndroidFX
+)
 
-#rm -rf "${mf[@]}"
+rm -rf "${mf[@]}"
 
-#echo "Old repositories has been removed"
+echo "Old repositories has been removed"
 
 # Clone new device specific repos
 
 # Deivce Tree
-#git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b yaap device/xiaomi/munch
+git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b yaap device/xiaomi/munch
 
 # Vendor Tree
-#git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
+git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
 # Munch FW
-#git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree (N0Kernel)
-#git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch kernel/xiaomi/munch
+git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b yaap-munch kernel/xiaomi/munch
 
 # Hardware Xiaomi
-#git clone https://github.com/PocoF4Trees/hardware_xiaomi -b 16 hardware/xiaomi
+git clone https://github.com/PocoF4Trees/hardware_xiaomi -b 16 hardware/xiaomi
 
 # Hardware Dolby Atmos
-#git clone https://github.com/PocoF4Trees/hardware_dolby hardware/dolby
+git clone https://github.com/PocoF4Trees/hardware_dolby hardware/dolby
 
 # DeviceSettings
-#git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
+git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
 
 # MIUI Camera
-#git clone https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
 # ViPER4AndroidFX
-#git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
 # Build
-#rm -rf device/qcom/common
-#git clone https://github.com/Olzhas-Kdyr/device_qcom_common.git device/qcom/common
-rm -rf bionic
-git clone https://github.com/Olzhas-Kdyr/bionic-yaap.git bionic
-
 . build/envsetup.sh
 lunch yaap_munch-user
 m yaap
