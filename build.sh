742c6c78a21ff13bac555bca35a31c562864a61a
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-08-03 00:29:04 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # repo init
-repo init -u https://github.com/AOSPA/manifest -b vauxite
+repo init -u https://github.com/peternmuller/manifest -b vauxite
 
 rm -rf prebuilts/clang/host/linux-x86
 
@@ -19,9 +19,12 @@ hardware/xiaomi
 rm -rf "${mf[@]}"
 
 # Deivce Tree
-git clone device/xiaomi/munch
+git clone https://github.com/Olzhas-Kdyr/aospa_device_xiaomi_munch -b vauxite device/xiaomi/munch
 
 # Kernel Tree
-git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b yaap-munch kernel/xiaomi/munch
+git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b aospa-munch kernel/xiaomi/munch
 
 # Build
+rm -rf vendor/qcom/common
+git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_qcom_common vendor/qcom/common
+bash rom-build.sh munch
